ad31a220147f493d892be9b51cca0e7ce82ab106
Theo Pack Test <tf.pack@gmail.com>
2016-09-25 19:07:04 +0200

    Add startup script and readme

---
+++ container_start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+docker run -d \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -p 57575:57575 -p 3000:3000 \
+    furikuri/docker-workshop:latest 1>&2
+
+if [ $? -ne "0" ]; then
+   echo "Error starting container: $?" 1>&2
+   exit 1
+fi
+
+sleep 5
+
+echo "Slides are available under: http://localhost:3000/"
