5c39f201d587e91c4a81dcbfca83c1167b499216
Theo Pack <theo.pack@cologne-intelligence.de>
2016-10-21 09:19:56 +0200

    Only allow access from local

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker run -d \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -p 57575:57575 -p 3000:3000 \
+    -p 57575:57575 -p 127.0.0.1:3000:3000 \
     furikuri/docker-workshop:latest 1>&2
 
 if [ $? -ne "0" ]; then
